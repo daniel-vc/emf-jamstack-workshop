ee5d7c8a1713ae67d173ee170bc5d23b3e2a8cd1
Dan VC <danvc@ellenmacarthurfoundation.org>
2020-04-14 14:36:34 +0100

    Updated the index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ const Index = () => {
   return (
     <>
       <h1>EMF CI/CD Workshop</h1>
+      <p>This should show up in the new build!</p>
       <p>This is the project for our workshop on Gatsby Cloud and GitHub Actions</p>
       <p>Use this project for the workshop</p>
     </>
